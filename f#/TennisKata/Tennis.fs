47ff03fcd13f7877acead3504e5986b76cd2d942
brunobelmondo <bruno.belmondo@gmail.com>
2015-06-25 14:28:50 +0200

    initial commit

---
+++ f#/TennisKata/Tennis.fs
@@ -0,0 +1,65 @@
+﻿namespace TennisKata
+
+module Tennis =
+    type Names = {Player1Name : string; Player2Name : string}
+    type Points = {Player1Score:int; Player2Score:int}
+    type GameStatus = {Names:Names;Points:Points}
+
+    let EmptyGame names = { Names=names; Points={Player1Score = 0; Player2Score = 0}; }
+
+    let AddPointToPlayer1 game = { Names = game.Names; Points = {Player1Score = game.Points.Player1Score + 1; Player2Score = game.Points.Player2Score} }
+    let AddPointToPlayer2 game = { Names = game.Names; Points = {Player1Score = game.Points.Player1Score; Player2Score = game.Points.Player2Score + 1} }
+
+    let CountPoint game player =
+        let names = game.Names
+        match player with
+        | x when x = names.Player1Name -> AddPointToPlayer1 game
+        | x when x = names.Player2Name -> AddPointToPlayer2 game
+        
+    let Game names points = 
+        let game = EmptyGame names
+        List.fold CountPoint game points
+
+    let PlayerScore score =
+        match score with
+        | 0 -> "Love"
+        | 1 -> "Fifteen"
+        | 2 -> "Thirty"
+        | 3 -> "Fourty"
+
+    let (|Win|_|) gameStatus =
+        let points = gameStatus.Points
+        if System.Math.Abs(points.Player1Score - points.Player2Score) > 1 &&
+           System.Math.Max(points.Player1Score, points.Player2Score) > 3 then
+            Some(Win)
+        else
+            None 
+
+    let (|Advantage|_|) gameStatus =
+        let points = gameStatus.Points
+        if System.Math.Abs(points.Player1Score - points.Player2Score) = 1 &&
+           System.Math.Max(points.Player1Score, points.Player2Score) > 3 then
+            Some(Advantage)
+        else
+            None 
+
+    let (|Deuce|_|) gameStatus =
+        let points = gameStatus.Points
+        if points.Player1Score = points.Player2Score && points.Player1Score > 3 then
+            Some(Deuce)
+        else
+            None
+
+    let GetLeaderName gameStatus =
+       let points = gameStatus.Points
+       if points.Player1Score > points.Player2Score then
+            gameStatus.Names.Player1Name
+        else
+            gameStatus.Names.Player2Name
+
+    let Score game = 
+        match game with
+        | Win -> "Win " + GetLeaderName game
+        | Advantage -> "Advantage " + GetLeaderName game
+        | Deuce -> "Deuce"
+        | _ -> PlayerScore game.Points.Player1Score + " " + PlayerScore game.Points.Player2Score
---
+++ f#/TennisKata/TennisTest.fs
@@ -0,0 +1,54 @@
+﻿namespace TennisKata
+
+open NUnit.Framework
+open Tennis
+
+[<TestFixture>]
+type TennisTests() =
+    let player1 = "player1"
+    let player2 = "player2"
+
+    let NamedGame points= Game {Player1Name=player1;Player2Name=player2} points
+
+    let BuildScore (player1Points,player2Points) =
+        let player1Points = List.replicate player1Points player1
+        let player2Points = List.replicate player2Points player2
+        List.append player1Points player2Points
+        
+    [<TestCase(0,0, Result="Love Love")>]
+    member x.Should_score_be_Love_Love_when_no_points(player1,player2) =
+        BuildScore (player1,player2) |> NamedGame |> Score
+
+    [<TestCase(1,0, Result="Fifteen Love")>]
+    [<TestCase(0,1, Result="Love Fifteen")>]
+    member x.Should_score_be_Fifteen_when_one_points(player1,player2) =
+        BuildScore (player1,player2) |> NamedGame |> Score
+ 
+    [<TestCase(2,0, Result="Thirty Love")>]
+    [<TestCase(0,2, Result="Love Thirty")>]
+    member x.Should_score_be_Thirty_when_two_points(player1,player2) =
+        BuildScore (player1,player2) |> NamedGame |> Score
+
+    [<TestCase(3,0, Result="Fourty Love")>]
+    [<TestCase(0,3, Result="Love Fourty")>]
+    member x.Should_score_be_Fourty_when_three_points(player1,player2) =
+        BuildScore (player1,player2) |> NamedGame |> Score
+
+    [<TestCase(4,4, Result="Deuce")>]
+    [<TestCase(5,5, Result="Deuce")>]
+    member x.Should_score_be_Deuce_when_equality_and_score_at_least_4(player1,player2) =
+        BuildScore (player1,player2) |> NamedGame |> Score
+
+    [<TestCase(4,3, Result="Advantage player1")>]
+    [<TestCase(5,4, Result="Advantage player1")>]
+    [<TestCase(3,4, Result="Advantage player2")>]
+    [<TestCase(4,5, Result="Advantage player2")>]
+     member x.Should_score_be_Advantage_when_score_difference_is_1_and_a_player_scores_4_or_more(player1,player2) =
+        BuildScore (player1,player2) |> NamedGame |> Score
+
+    [<TestCase(4,2, Result="Win player1")>]
+    [<TestCase(5,3, Result="Win player1")>]
+    [<TestCase(2,4, Result="Win player2")>]
+    [<TestCase(2,5, Result="Win player2")>]
+     member x.Should_score_be_Win_when_score_difference_is_2_and_a_player_scores_4_or_more(player1,player2) =
+        BuildScore (player1,player2) |> NamedGame |> Score
